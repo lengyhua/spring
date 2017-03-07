2187a6162e6f8b14fcfb968c0c563382dd13ef89
zhaobing0410 <zhaobing0410@163.com>
2017-03-07 11:57:13 +0800

    我的测试

---
+++ PorjectZhao/src/com/zhao/Test/MyTest.java
@@ -0,0 +1,21 @@
+package com.zhao.Test;
+
+	/**
+	 * 
+	 * @类功能说明：  
+	 * @类修改者：  
+	 * @修改日期：  
+	 * @修改说明：  
+	 * @公司名称：ultrapower.com.cn 
+	 * @作者：zhaobing0410  
+	 * @创建时间：2017年3月7日 上午11:52:31  
+	 * @版本：V1.0
+	 */
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
